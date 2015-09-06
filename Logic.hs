f8a2703104acba523a591773fdf977312ca0c12f
Vladimir Pankov <wowofbob@gmail.com>
2015-09-06 19:28:08 +0300

    Did some attempts with ordering productions

--- Logic.hs
+++ Logic.hs
@@ -2,6 +2,7 @@
 
 import qualified Data.Set as DS 
 import qualified Data.Map as DM
+import qualified Data.Vector as DV
 
 import Control.Monad.State
 import Control.Monad.Trans.Maybe
@@ -9,43 +10,71 @@ import Control.Monad.Trans.Maybe
 import Data.Maybe
 
 
-
-type Object = String
-  
-data Fact = Fact String [Object]
-  deriving (Eq, Ord, Show)
-
-fact2atom :: Fact -> Formula
-fact2atom (Fact x y) = Atom x y
-
-data Formula = Atom String [Object] | And Formula Formula | Or Formula Formula
-  deriving (Eq, Ord, Show)
-
-type Antecedent = Formula
-type Succedent  = Fact
-data Rule = Antecedent :=> Succedent
+-- Looks like it's possible to use strings as facts.
+-- There is no need in predicates for reasoning. 
+newtype Fact = Fact { factToString :: String }
   deriving (Eq, Ord)
+-- Fact's recency data
+newtype Recency = Recency { recencyToInt :: Int } -- +1 to current time step  
+type FactRecency = DM.Map Fact Recency
   
-ruleAnt (ant :=> _) = ant
-ruleSuc (_ :=> suc) = suc
+-- Production's concreteness
+newtype Concr = Concr { concrToInt :: Int } -- number of production's conditions
+-- Production's priority
+newtype Prio = Prio { prioToInt :: Int } -- value to set up by expert
+-- Production's successful usage value when proving some goal (usefulness)
+newtype Usef = Usef { usefToInt :: Int } -- number of successful usages
 
-formula2facts :: Formula -> [Fact]
-formula2facts (Atom name args) = [Fact name args]
-formula2facts (And a b) = [a, b] >>= formula2facts
-formula2facts (Or  a b) = [a, b] >>= formula2facts
+-- ID of production
+type ProdID = Int
 
+-- Actions
+data Action = Assert [Fact] -- adds list of facts to a working memory
+            | Terminate     -- terminates inferring
+            deriving (Eq, Ord)
 
-type Fact2Rules = DM.Map Fact [Rule]
+type Goal = [Action]
+  
+-- Data about successful usage of production when proving some goal
+type ProdUsef = DM.Map (ProdID, Goal) Usef
 
-addRule :: Rule -> Fact2Rules -> Fact2Rules
-addRule r f2r = insertKeyNodes f2r [r] $ formula2facts $ ruleAnt r 
-  where  
-    insertKeyNodes m v = foldl (\ m k -> DM.insertWith (++) k v m) m
+-- Data about production's concretness
+type ProdConcr   = DV.Vector Concr
+-- Data about production's priority
+type ProdPrio    = DV.Vector Prio
+-- Data about production's conditions recency
+type ProdRecency = DV.Vector Recency
+  
+-- Productions manager. Keeps all data about productions.
+-- Can solve what production to apply.
+data ProdManager = ProdManager Int ProdConcr ProdPrio ProdRecency ProdUsef
 
-relatedRules :: Fact2Rules -> [Fact] -> [Rule]
-relatedRules f2r = catMaybes . map (lookupRule f2r) 
+-- Combines Usef Concr Prio and Recency into score
+combine :: Concr -> Prio -> Recency -> Usef -> Int
+combine (Concr c) (Prio p) (Recency r) (Usef u) =
+  c + p + r + u
+
+resolve :: ProdManager -> Goal -> ProdID
+resolve (ProdManager n pc pp pr pu) goal =
+  -- getting scores of productions
+  let scores = DV.generate n
+        (\ pId -> combine (pc DV.! pId)
+                          (pp DV.! pId)
+                          (pr DV.! pId)
+                          (puVec DV.! pId))
+                            in DV.maxIndex scores 
   where
-    lookupRule m f = fmap head $ DM.lookup f m
+    -- Convertion of map `pu` into vector
+    puVec = DV.fromListN n $ map
+      (\ pId -> case DM.lookup (pId, goal) pu of
+                  Just a  -> a
+                  Nothing -> Usef 0) [1..n]
 
+initManager :: FactRecency -> ProdUsef 
 
+  
+data WorkMemElem =
+  WMElem { fact     :: Fact
+         , recency  :: Int
+         , priority :: Int }
 
